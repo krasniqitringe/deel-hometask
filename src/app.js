d2f0ebcdbeb3db75ba651eac47aaf3603c3efa35
krasniqitringe <147447568+krasniqitringe@users.noreply.github.com>
2023-11-30 10:26:20 +0100

    Fixed API get contract by id [#1]

--- src/app.js
+++ src/app.js
@@ -7,15 +7,39 @@ app.use(bodyParser.json());
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
+const { Op } = require('sequelize');
+
+
 /**
  * FIX ME!
  * @returns contract by id
  */
-app.get('/contracts/:id',getProfile ,async (req, res) =>{
-    const {Contract} = req.app.get('models')
-    const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
-    if(!contract) return res.status(404).end()
-    res.json(contract)
+
+app.get('/contracts/:id', getProfile, async (req, res) => {
+    const { Contract } = req.app.get('models');
+    const { id } = req.params;
+    const profile_id = req.profile.dataValues.id;
+
+    try {
+        const contract = await Contract.findOne({
+            where: {
+                [Op.and]: [
+                    { [Op.or]: [{ ClientId: profile_id }, { ContractorId: profile_id }] },
+                    {id: id}
+                ],
+            },
+        });
+
+        if (!contract) {
+            return res.status(404).json({ error: 'Contract not found!' });
+        }
+
+        res.status(200).json(contract);
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+
 })
+
 module.exports = app;
